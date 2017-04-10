fbf3e9ff93438d172eab0d42f721dc479b227ad6
Dmitrijs Balcers <dbalcers@evolutiongaming.com>
2017-04-11 00:37:46 +0200

    Fix everything related to instantclick

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ var webWorker1Result = document.getElementsByClassName("web-worker1-result")[0];
 
 if(typeof(Worker) !== "undefined") {
   if(typeof(w) === "undefined") {
+    console.log("Start WebWorker");
     w = new Worker("src/web-workers/index.js");
   }
   w.onmessage = function(e) {
@@ -11,3 +12,11 @@ if(typeof(Worker) !== "undefined") {
 } else {
   console.error("This browser does not support Web Workers. You really should update your browser.");
 }
+
+InstantClick.on('change', function() {
+  if (w) {
+    console.log("Terminate WebWorker");
+    w.terminate();
+  }
+  w = undefined;
+});
